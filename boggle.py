7fd9709f60498aadf6187687ae05c0097e134be5
alexlyttle <a.j.lyttle@outlook.com>
2021-02-22 13:35:25 +0000

    Improved formatting

--- boggle.py
+++ boggle.py
@@ -7,73 +7,149 @@ This code is a work in progress.
 The code describes a Boggle-like computer game.
 """
 
-import os, time, threading
-from random import choice
-from numpy import random
+import os, time, threading, shutil, sys
+from typing import Optional, Sequence
+from random import choice, choices
 from pyfiglet import Figlet
 
 
+class BoardWidthError(Exception):
+    pass
+
+
 class Board:
+    """
+    Class for the Boggle board.
+
+    Parameters
+    ----------
+    size : int, optional
+        The size, N for an N x N board. Default is 4.
+    letters : Sequence of str or str, optional
+        If letters is not None, size will be ignored. The Sequence must have
+        length equal to a square number. Default is None.
+    p_vow : float, optional
+        Probability of a letter on the board being a float. Not used if letter
+        is not None. Default is 0.38.
+    p_con : float, optional
+        Probability of a letter on the board being a float. Not used if letter
+        is not None. Default is 0.62.
+
+    Attributes
+    ----------
+    size : int
+    p_vow : float
+    p_con : float
+    letters : list of str
+    adjacency : dict of 
+    found_words
+
+    Methods
+    -------
+
+    """
 
     _vowels = list('AEIOU')
     _consonants = list('BCDFGHJKLMNPQRSTVWXYZ')
+    _consonants[_consonants.index('Q')] += 'U'
 
-    def __init__(self, size=4, letters=None, p_vow=0.3, p_con=0.7):
-        """
-        Initialise a Board board.
-        :param int size: Size, N, for an N x N board
-        :param letters: Optional list of letters to fill the board
-        :param float p_vow: Probability of there being a vowel on the board
-        :param float p_con: Probability of there being a consonant on the board
-        """
+    def __init__(self, size: Optional[int]=4, 
+                 letters: Optional[Sequence[str]]=None, 
+                 p_vow: Optional[float]=None, p_con: Optional[float]=None):
         self.size = size
-        self.letters = [self.random_letter(p_vow, p_con) for _ in range(size*size)] if not letters else letters.upper()
+        self.p_vow = p_vow or 0.38
+        self.p_con = p_con or 0.62
+        self.letters = self._validate_letters(letters)
+        
+        if letters is not None:
+            size = len(self.letters)**0.5
+            if size % 1 != 0:
+                raise ValueError('Length of letters is not a square number.')
+            self.size = int(size)
 
-        if self.size*self.size != len(self.letters):
-            raise ValueError('Boggle board is not square: Please check letters is equal to the size squared.')
-
-        self._separator = ' | '
-        self.width = self.size * (len(self._separator) + 1) - len(self._separator)
-        rows = map(self._separator.join, zip(*[iter(self.letters)] * self.size))
-        self._str = ('\n'+'-'*self.width+'\n').join(rows)
-
+        sep = '|||'
+        wrap = '||'
+        self._width = self.size * (len(sep) + 3) - len(sep) + 2 * len(wrap)
+        rows = map(
+            lambda row: wrap + \
+                sep.join([f'{r:<3}' for r in row]) + wrap,
+            zip(*[iter(self.letters)] * self.size)
+        )
+        # self._str = ('\n'+'-'*self._width+'\n').join(rows)
+        row_head = (' ' + '_' * (len(sep) + 2)) * self.size + '\n'
+        row_sep = '\n|' + ('|' + '___' + '||') * self.size + '\n'
+        row_foot = '|' + ('/' + '___' + '\\|') * self.size
+        self._str = row_head + row_sep.join(rows) + row_sep + row_foot
         self.adjacency = self._init_adjacency()
         self.found_words = list()
+
+        self._check_width()
 
     def __str__(self):
         return self._str
 
+    def _check_width(self):
+        if self._width > Boggle._width:
+            raise BoardWidthError('Board width exceeds terminal window width.')
+
+    def _validate_letter(self, letter):
+        if isinstance(letter, str):
+            if letter in self._vowels or letter in self._consonants:
+                return letter.upper()
+            else:
+                msg = f'Character {letter} is not a vowel or consonant.'
+                raise ValueError(msg)
+        else:
+            raise TypeError(f'Letter {letter} is not an instance of str.')
+
+    def _validate_letters(self, letters):
+        if isinstance(letters, Sequence):
+            letters = [self._validate_letter(letter) for letter in letters]
+        elif letters is None:
+            letter_choices = self.choose_letters(k=self.size**2)
+            letters = [c() for c in letter_choices]
+        else:
+            raise TypeError('Variable letters is not a Sequence or None.')
+        return letters
+
     def _init_adjacency(self):
         adjacency = {}
-        for i in range(self.size*self.size):
+        for i in range(self.size**2):
             x, y = divmod(i, self.size)
             adjacency[i] = adj = []
-            if x > 0:  # If row is not first, add above element index
+            if x > 0: 
+                # If row is not first, add above element index
                 adj.append(i - self.size)
-                if y > 0:  # If column is not first, add northwest element index
+                if y > 0:
+                    # If column is not first, add northwest element index
                     adj.append(i - self.size - 1)
-                if y < self.size - 1:  # If column is not last, add northeast element index
+                if y < self.size - 1:
+                    # If column is not last, add northeast element index
                     adj.append(i - self.size + 1)
-            if x < self.size - 1:  # If row is not last, add below element index
+            if x < self.size - 1:
+                # If row is not last, add below element index
                 adj.append(i + self.size)
-                if y > 0:  # If column is not first, add southwest element index
+                if y > 0:
+                    # If column is not first, add southwest element index
                     adj.append(i + self.size - 1)
-                if y < self.size - 1:  # If column is not last, add southeast element index
+                if y < self.size - 1:
+                    # If column is not last, add southeast element index
                     adj.append(i + self.size + 1)
-            if y > 0:  # If column is not first, add left element index
+            if y > 0:
+                # If column is not first, add left element index
                 adj.append(i - 1)
-            if y < self.size - 1:  # If column is not last, add right element index
+            if y < self.size - 1:
+                # If column is not last, add right element index
                 adj.append(i + 1)
         return adjacency
 
-    def random_letter(self, p_vow, p_con):
+    def choose_letters(self, k=1):
         """
-        Return a random letter for a given distribution of vowels and consonants.
-        :param float p_vow: Probability of returning a vowel
-        :param float p_con: Probability of returning a consonant
-        :return string: Random letter
+        Choose vowel or consonant letter types.
         """
-        return random.choice((choice(self._vowels), choice(self._consonants)), p=(p_vow, p_con))
+        return choices([lambda: choice(self._vowels), 
+                        lambda: choice(self._consonants)], 
+                       weights=[self.p_vow, self.p_con], k=k)
 
     def find_words(self, words):
         """
@@ -85,19 +161,30 @@ class Board:
         
         found = list()
 
-        def advance_path(prefix, path):
-            """ Advance the path of possible words given the prior prefix and path indices. """
-            if prefix in words:  # If the prefix is in list of words, add to found words
+        def _advance_path(prefix, path):
+            """
+            Advance the path of possible words given the prior prefix and path
+            indices.
+            """
+            if prefix in words:
+                # If prefix is in list of words, add to found words
                 found.append(prefix)
-            if prefix in prefixes:  # If the prefix is in the list of prefixes, advance the path
+            if prefix in prefixes:
+                # If prefix is in the list of prefixes, advance the path
                 for j in self.adjacency[path[-1]]:
                     # For each adjacent letter to the last element in the path
-                    if j not in path:  # If the element is not already in the path, advance path
-                        advance_path(prefix + self.letters[j], path + [j])
+                    if j not in path:
+                        # If element is not already in the path, advance path
+                        _advance_path(prefix + self.letters[j], path + [j])
 
         for (i, letter) in enumerate(self.letters):
             # For each letter in the list of board letters, start a path
-            advance_path(letter, [i])
+            sys.stdout.write('\r')
+            sys.stdout.write(
+                'Finding words on board {:2.0f}%'.format(100*i/self.size**2)
+            ) 
+            sys.stdout.flush()
+            _advance_path(letter, [i])
         self.found_words = found
 
 
@@ -120,7 +207,8 @@ class Player:
             self.score += Boggle.find_score(guess)
             self.found_words.append(guess)
         elif guess in self.found_words:
-            self.message = 'You have already entered this word, please try again!'
+            self.message = 'You have already entered this word, ' + \
+                'please try again!'
         elif guess is not None:
             self.message = 'Invalid word, try again!'
 
@@ -136,20 +224,23 @@ class Player:
 
 
 class Boggle:
-    _points = {3: 1, 4: 2, 5: 4, 6: 6, 7: 9, 8: 15}
+    _points: dict = {3: 1, 4: 2, 5: 4, 6: 6, 7: 9, 8: 15}
 
-    _indent = 24
-    # _plural = lambda point: 's' if point > 1 else ''
-    _wordrows = {}  # {k: f'{f"{k} letters ({v} point{_plural(v)})":<{_indent-2}}: ' for k, v in _points.items()}
+    _indent: int = 24
+    _wordrows: dict = {}  
     for k, v in _points.items():
         plural = 's' if v > 1 else ''
         _wordrows[k] = f'{f"{k} letters ({v} point{plural})":<{_indent-2}}: '
-    _width = 120
-    _div = _width * '='
-    _figlet = Figlet(font='smkeyboard', width=_width)
-    _title = _figlet.renderText('Boggle')
+        del plural
+    _width: int = max(shutil.get_terminal_size()[0], 80)
+    # _height: int = shutil.get_terminal_size()[1]
+    _div: str = _width * '='
+    _figlet: Figlet = Figlet(font='smkeyboard', width=_width)
+    _title: str = _figlet.renderText('Boggle')
 
-    def __init__(self, player=None, board=None, time_limit=120, words_file='words.txt'):
+    def __init__(self, player: Optional[Player]=None,
+                 board: Optional[Board]=None, time_limit: int=120,
+                 words_file: str='words.txt'):
         """
         Initialise the game.
         :param player: Player
@@ -160,14 +251,15 @@ class Boggle:
         self.board = board or Board()
         self.timer = threading.Timer(time_limit, self.stop)
         self.timer.daemon = True  # Timer thread closes with main thread
-
+        
         words = self.load_words(words_file)
         self.board.find_words(words)
 
     @staticmethod
     def load_words(file, min_len=3):
         """
-        Loads a dictionary of words from a given file or string and returns all words and prefixes.
+        Loads a dictionary of words from a given file or string and returns all
+        words and prefixes.
         :param file: File location or string for words
         :param int min_len: Optional minimum length of words
         :return tuple: A 2-tuple of words and prefixes
@@ -177,14 +269,15 @@ class Boggle:
                 # Strip off whitespace at end of line and capitalize
                 word = line.rstrip().upper()
                 if len(word) >= min_len:
-                    # If the word length is at least the minimum, yield word in iteration
+                    # If word length is at least the minimum,
+                    # yield word in iteration
                     yield word
 
         if isinstance(file, str):
             with open(file) as f:
-                words = set(load(f))
+                words = list(load(f))
         else:
-            words = set(load(file))
+            words = list(load(file))
         # Finds set of prefixes for each word
         return words
 
@@ -224,7 +317,8 @@ class Boggle:
             total_lines = len(value)//words_per_line + 1
 
             indices = [i*words_per_line for i in range(total_lines)]
-            lines = [separator.join(value[i:j]) for i, j in zip(indices, indices[1:]+[None])]
+            lines = [separator.join(value[i:j]) for i, j in \
+                zip(indices, indices[1:]+[None])]
             
             s += (separator+'\n'+' '*self._indent).join(lines)
             blocks.append(s)
@@ -259,7 +353,11 @@ class Boggle:
         print(self.format_words(self.player.found_words))
         print(self._div)
         print('You missed the following words:')
-        missed_words = list(set(self.board.found_words).difference(set(self.player.found_words)))
+        missed_words = list(
+            set(self.board.found_words).difference(
+                set(self.player.found_words)
+            )
+        )
         print(self.format_words(missed_words))
 
     def start(self):
